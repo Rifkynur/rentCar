7661ec65e75a22f2aefe385a601d2bd87f111474
Rifkynur <ythrifky619@gmail.com>
2025-03-18 21:09:09 +0700

    first commit

---
+++ app/Models/Car.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Car extends Model
+{
+    use SoftDeletes,HasFactory;
+
+    protected $fillable = ['user_id','police_number','merk','type','capacity','price','photo'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
